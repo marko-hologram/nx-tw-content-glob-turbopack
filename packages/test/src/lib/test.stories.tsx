056e042269e4348f89ee4fd13c7c5a1786d06213
Marko Antolić <22904204+marko-hologram@users.noreply.github.com>
2025-02-09 19:43:04 +0100

    Commit

---
+++ packages/test/src/lib/test.stories.tsx
@@ -0,0 +1,3 @@
+export const ComponentFromStoriesFileInPackage = () => {
+  return <div className="p-20 bg-purple-400">Component from stories file in package</div>
+}
---
+++ packages/test/src/lib/test.tsx
@@ -0,0 +1,3 @@
+export const MyTestComponent = () => {
+  return <div className="p-20 bg-purple-400">My Test Component</div>
+}
